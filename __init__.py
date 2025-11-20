1d35f6e04c5b711bbd667b6d5e1ab44ebbc18cb2
RajvanshMalhotra <2004rajvansh@gmail.com>
2025-11-20 08:41:34 +0530

    Start __init__.py: Small fix in __init__.py

--- __init__.py
+++ __init__.py
@@ -10,14 +10,3 @@ from .console import (
     get_input
 )
 
-__all__ = [
-    'console',
-    'print_intro',
-    'print_result',
-    'print_success',
-    'print_warning',
-    'print_error',
-    'print_info',
-    'print_dim',
-    'get_input'
-]
